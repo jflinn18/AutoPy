b23f070987501c27ffcc6d08cbccb23473647809
jflinn18 <joeflinn412@gmail.com>
2015-06-26 15:15:13 -0700

    Added a New Repository
    Added my automation module

---
+++ Automation.py
@@ -0,0 +1,8 @@
+import os
+
+
+i = 0
+
+while i < 5:
+	os.system('python numExp.py')
+	i = i + 1
---
+++ numExp.py
@@ -0,0 +1,2 @@
+num = raw_input('Enter a number: ')
+print str(int(num) + 10) + ' is your number plus 10\n'
